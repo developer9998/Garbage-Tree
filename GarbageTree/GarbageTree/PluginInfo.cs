9d16fdd65f8ae9a45c27284b2f0c284c705cfe17
devwhitty <81720436+DeveloperWhitty@users.noreply.github.com>
2022-04-21 21:05:14 -0230

    v1.0.0

---
+++ GarbageTree/GarbageTree/PluginInfo.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Garbage
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.dev9998.gorillatag.Garbage";
+        public const string Name = "Garbage Tree";
+        public const string Version = "1.0.0";
+    }
+}
